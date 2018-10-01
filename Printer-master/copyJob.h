46824d1f139a1214140c7352435185a404e986cf
0dwalker <31480998+0dwalker@users.noreply.github.com>
2018-10-01 14:00:40 -0500

    Add files via upload

---
+++ Printer-master/copyJob.h
@@ -0,0 +1,9 @@
+#ifndef COPYJOB
+#define COPYJOB
+//Define a user
+typedef struct{
+} CopyJob;
+char FileName[40];
+int pages;
+char cont[525];
+#endif //COPYJOB
---
+++ Printer-master/user.h
@@ -0,0 +1,9 @@
+#ifndef USER
+#define USER
+//Define a user
+typedef struct{
+long PageCount;
+char username[20];
+} User;
+#endif //USER
+
